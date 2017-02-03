38e6c53b365bb7d3dce0f909639913555f303d7b
Ben Hughes <bwghughes@gmail.com>
2017-02-03 03:36:04 +0000

    basic functionality

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from tinydb import TinyDB, Query
+
 app = Flask(__name__)
 
+db = TinyDB('passengers.json')
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    passenger_data = db.all()
+    return render_template('index.html', passenger_data=passenger_data)
+
+@app.route("/save_number", methods=['POST'])
+def save_number():
+    db.insert({'number': request.form.get('number')})
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
